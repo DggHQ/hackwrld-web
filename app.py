5a18dedb21e20ebaf653c17f0ef28148fb54a639
Kevin Christen <kevin.christen@grabx.ch>
2024-04-12 00:29:03 +0200

    add initial code

---
+++ app.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python3
+from flask import Flask, request, redirect, url_for, session, render_template
+from functools import wraps
+import requests
+import urllib.parse
+from hashlib import sha256
+import uuid
+import base64
+import string
+import random
+import json
+import os
+
+from k8s import *
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "supersecretkey")
+
+callback_url_setting = os.getenv("CALLBACK_URL", "http://localhost:8888/callback")
+app_id_setting = os.getenv("APP_ID")
+app_secret_setting = os.getenv("APP_SECRET")
+app_websocket_url = os.getenv("WS_URL", "ws://localhost:8080/ws")
+app_namespace = os.getenv("NAMESPACE", "hackwrld")
+app_nats_host = os.getenv("NATS_HOST")
+app_etcs_endpoints = os.getenv("ETCD_ENDPOINTS")
+
+
+CHALLENGES = {}
+app_id = app_id_setting
+secret = app_secret_setting
+callback_url = urllib.parse.quote(callback_url_setting)
+
+
+def b64_encode(code_verifier, hashed_secret):
+    val = code_verifier + hashed_secret
+    return_value_hex = sha256(val.encode('ascii')).hexdigest()
+    return base64.b64encode(return_value_hex.encode('ascii')).decode('ascii')
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "userdata" not in session:
+            return redirect(url_for('auth', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route("/auth")
+def auth():
+    hashed_secret = sha256(secret.encode('ascii')).hexdigest()
+    code_verifier = ''.join(random.choices(
+        string.ascii_letters+string.digits,
+        k=43))
+    code_challenge = b64_encode(code_verifier, hashed_secret)
+    state = str(uuid.uuid4())
+    CHALLENGES[state] = code_verifier
+    print(f"state: {state}, verifier: {CHALLENGES[state]}")
+    url = f"https://www.destiny.gg/oauth/authorize?response_type=code&client_id={app_id}&redirect_uri={callback_url}&state={state}&code_challenge={code_challenge}"
+    print(f"callback url: {url}")
+    return redirect(url, code=302)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    session.pop("userdata")
+    return "Logged out", 200
+
+
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    state = request.args.get("state")
+    code_verifier = CHALLENGES[state]
+    auth_url = f"https://www.destiny.gg/oauth/token?grant_type=authorization_code&code={code}&client_id={app_id}&redirect_uri={callback_url}&code_verifier={code_verifier}"
+    auth_response = requests.get(auth_url)
+    data = auth_response.json()
+
+    user_data_url = f"https://www.destiny.gg/api/userinfo?token={data['access_token']}"
+    user_response = requests.get(user_data_url)
+    userdata = user_response.json()
+    session["userdata"] = userdata
+    return redirect(url_for('prepare', userID=userdata["userId"]))
+
+
+# API call only
+@app.route("/cc/<userID>/create", methods=["POST"])
+@login_required
+def create_cc(userID):
+    if str(session["userdata"]["userId"]) != userID:
+        return json.dumps({"error": "unauthicated"}), 403
+    '''
+    Create command center deployment for user if it does not exist.
+    '''
+    ips = get_cc_ip(
+        userId=userID,
+        namespace=app_namespace
+    )
+    # Redirect directly if command center exists.
+    if len(ips) > 0:
+        return json.dumps({"success": "already exists"}), 200
+    
+    # Create deployment if no ips have been returned
+    uuid_value = str(uuid4())
+    config.load_config()
+    deployment = create_deployment_object(
+        nats_host=app_nats_host,
+        etcd_endpoints=app_etcs_endpoints,
+        deployment_name=uuid_value
+    )
+    v1 = client.AppsV1Api()
+    v1.create_namespaced_deployment(
+        body=deployment, namespace=app_namespace
+    )
+    deploy, statuscode  = v1.read_namespaced_deployment_with_http_info(
+        name=uuid_value,
+        namespace=app_namespace
+    )
+    print(statuscode)
+    if statuscode != "200":
+        return json.dumps({"error": "not_ready"}), 403
+    else:
+        return json.dumps({"success": "ready"}), 200
+
+@app.route("/cc/<userID>/prepare")
+@login_required
+def prepare(userID):
+    '''
+    Endpoint that shows a loading screen while the containers
+    are being started.
+    Contains a website with a javascript that periodically
+    should check connection.
+    On manual retry it should just straight go to the game.
+    '''
+    if str(session["userdata"]["userId"]) != userID:
+        return "Error: Unauthorized User", 403
+    return render_template("getready.html", userid=userID)
+
+
+@app.route("/state")
+@login_required
+def state():
+    # Query backend pod 
+    # Get pod (with label selectors) ip via k8s api first then get the state
+    ips = get_cc_ip(
+        userId=str(session["userdata"]["userId"]),
+        namespace=app_namespace
+    )
+    commandCenterIP = f"http://{ips[0]['ip']}/state"
+    state = requests.get(commandCenterIP).json()
+    return json.dumps(state)
+
+@app.route("/upgrade/firewall", methods=["POST"])
+@login_required
+def upgrade_firewall():
+    # Query backend pod 
+    # Get pod (with label selectors) ip via k8s api first then get the state
+    ips = get_cc_ip(
+        userId=str(session["userdata"]["userId"]),
+        namespace=app_namespace
+    )
+    commandCenterIP = f"http://{ips[0]['ip']}/upgrade/firewall"
+    data = requests.post(commandCenterIP)
+    state = data.json()
+    statusCode = data.status_code
+    return json.dumps(state), statusCode
+
+@app.route("/upgrade/scanner", methods=["POST"])
+@login_required
+def upgrade_scanner():
+    # Query backend pod 
+    # Get pod (with label selectors) ip via k8s api first then get the state
+    ips = get_cc_ip(
+        userId=str(session["userdata"]["userId"]),
+        namespace=app_namespace
+    )
+    commandCenterIP = f"http://{ips[0]['ip']}/upgrade/scanner"
+    data = requests.post(commandCenterIP)
+    state = data.json()
+    statusCode = data.status_code
+    return json.dumps(state), statusCode
+
+@app.route("/upgrade/stealer", methods=["POST"])
+@login_required
+def upgrade_stealer():
+    # Query backend pod 
+    # Get pod (with label selectors) ip via k8s api first then get the state
+    ips = get_cc_ip(
+        userId=str(session["userdata"]["userId"]),
+        namespace=app_namespace
+    )
+    commandCenterIP = f"http://{ips[0]['ip']}/upgrade/stealer"
+    data = requests.post(commandCenterIP)
+    state = data.json()
+    statusCode = data.status_code
+    return json.dumps(state), statusCode
+
+@app.route("/upgrade/miner", methods=["POST"])
+@login_required
+def upgrade_miner():
+    # Query backend pod 
+    # Get pod (with label selectors) ip via k8s api first then get the state
+    ips = get_cc_ip(
+        userId=str(session["userdata"]["userId"]),
+        namespace=app_namespace
+    )
+    commandCenterIP = f"http://{ips[0]['ip']}/upgrade/miner"
+    data = requests.post(commandCenterIP)
+    state = data.json()
+    statusCode = data.status_code
+    return json.dumps(state), statusCode
+
+@app.route("/scan/out", methods=["POST"])
+@login_required
+def init_scan():
+    # Query backend pod 
+    # Get pod (with label selectors) ip via k8s api first then get the state
+    ips = get_cc_ip(
+        userId=str(session["userdata"]["userId"]),
+        namespace=app_namespace
+    )
+    commandCenterIP = f"http://{ips[0]['ip']}/scan/out"
+    data = requests.post(commandCenterIP)
+    state = data.json()
+    statusCode = data.status_code
+    return json.dumps(state), statusCode
+
+
+@app.route("/cc/<userID>/home")
+@login_required
+def home(userID):
+    if str(session["userdata"]["userId"]) != userID:
+        return "Error: Unauthorized User", 403
+    return render_template("idx.html", userid=userID, websocket_url=app_websocket_url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8888)
---
+++ k8s.py
@@ -0,0 +1,68 @@
+from kubernetes import client, config
+import time
+from uuid import uuid4
+
+def create_deployment_object(requestor, nats_host, etcd_endpoints, deployment_name):
+    # Configureate Pod template container
+    image_pull_secret = [client.V1LocalObjectReference(name="regcred")]
+    container = client.V1Container(
+        name=deployment_name,    
+        image="harbor.0x01.host/dgg/voteworker",
+        env=[client.V1EnvVar(name="ID", value=requestor),
+             client.V1EnvVar(name="NATS_HOST", value=nats_host),
+             client.V1EnvVar(name="ETCD_ENDPOINTS", value=etcd_endpoints),
+             client.V1EnvVar(name="GIN_MODE", value="release")],
+        ports=[client.V1ContainerPort(container_port=80)],
+        resources=client.V1ResourceRequirements(
+            limits={"memory": "10Mi"},
+        ),
+    )
+
+    # Create and configure a spec section
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={
+                "app": deployment_name,
+                "uuid": deployment_name,
+                "id": requestor,
+                "app": "hackwrld-client",
+                "creationDate": str(int(time.time()))
+            },
+        ),
+        spec=client.V1PodSpec(containers=[container], image_pull_secrets=image_pull_secret),
+    )
+
+    # Create the specification of deployment
+    spec = client.V1DeploymentSpec(
+        replicas=1, template=template, selector={
+            "matchLabels":
+            {"app": deployment_name}})
+
+    # Instantiate the deployment object
+    deployment = client.V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=client.V1ObjectMeta(name=deployment_name, labels={
+            "id": requestor,
+            "app": "hackwrld-client",
+            "name": deployment_name,
+            "uuid": deployment_name
+        }),
+        spec=spec,
+    )
+    return deployment
+
+
+def get_cc_ip(userId, namespace) -> list:
+    config.load_config()
+    v1 = client.CoreV1Api()
+    mypods = v1.list_namespaced_pod(
+        namespace=namespace,
+        label_selector=f"id={userId}", 
+        watch=False
+    )
+    ccs = []
+    for pod in mypods.items:
+        ccs.append({
+            "ip": pod.status.pod_ip
+        })
+    return ccs
